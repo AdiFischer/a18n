c721bc01a0a3274d86b750af5ee86ad485a77379
AdiFischer <adifischer1@gmail.com>
2022-11-15 15:23:42 -0500

    added a Footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { useState, createContext} from 'react';
 import Details from './components/Details';
+import Footer from './components/Footer';
 import English from './lang/en.json'
 import Spanish from './lang/es.json'
 import French from './lang/fr.json'
 import './App.css';
 
 const locale = navigator.language || 'en';
-
+// more than 3 lang-use switch statement//
 const _lang = (locale==='es')
                  ? Spanish 
                  : (locale==='fr')
@@ -18,7 +19,7 @@ export const LanguageContext = createContext()
 function App() {
   const[lang, setLang] = useState(_lang)
   return (
-    <LanguageContext.Provider value={{ lang }}>
+    <LanguageContext.Provider value={{ lang, English }}>
     <div className="App">
       <header className="App-header">
         <div>
@@ -28,8 +29,16 @@ function App() {
         </div>
        <h1>{lang["app.header"]}</h1>
         <p>{lang["app.subhead"]}</p>
-        <a>{lang["app.cta"]}</a>
-        < Details />
+        <a 
+        className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {lang["app.cta"]}
+        </a>
+        <Details />
+        <Footer />
       </header>
     </div>
     </LanguageContext.Provider>
